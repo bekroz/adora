29e9a980ab17c345476a2f6d7e1373389ff3253e
BEK ROZ <bekrozikoff@gmail.com>
2022-04-27 23:10:41 +0500

    [ADD] Homescreen

---
+++ app/constants/img/index.ts
@@ -0,0 +1,3 @@
+import type { Image } from 'react-native'
+
+import MainImage from '../../../assets/images/main.png'
---
+++ app/screens/private/HomeScreen/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    homeTitleContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    homeTitle: {
+        color: '#000000',
+        fontSize: 25,
+        fontWeight: '600',
+        fontFamily: 'Inter Regular',
+    }
+})
