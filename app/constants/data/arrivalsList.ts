abb44312ad4a06c35e3a3d628310364de6557fd6
BEK ROZ <bekrozikoff@gmail.com>
2022-05-03 23:38:21 +0500

    [REFACTOR] Single data object for global access

---
+++ app/constants/data/arrivalsList.ts
@@ -0,0 +1,28 @@
+const ARRIVALS_LIST = [
+  {
+    name: 'model_1',
+    description: '21WN reversible cardigan',
+    imgUri: require('../../assets/images/model_1.png'),
+    price: 109,
+  },
+  {
+    name: 'model_2',
+    description: 'Green stylish sweater',
+    imgUri: require('../../assets/images/model_2.png'),
+    price: 79,
+  },
+  {
+    name: 'model_3',
+    description: 'Angora gray one piece',
+    imgUri: require('../../assets/images/model_3.png'),
+    price: 159,
+  },
+  {
+    name: 'model_4',
+    description: 'Oblong bag',
+    imgUri: require('../../assets/images/model_4.png'),
+    price: 199,
+  },
+];
+
+export default ARRIVALS_LIST;
--- app/constants/data/index.ts
+++ app/constants/data/index.ts
@@ -1,120 +1,20 @@
-export const EXLORE_BUTTON_TITLE = 'Explore collection';
-export const ARRIVAL_TITLE = 'NEW ARRIVAL';
-export const ARRIVAL_CATEGORIES = ['All', 'Apparel', 'Dress', 'Tshirt', 'Bag'];
-export const EXPLORE_MORE = 'Explore More';
+import ARRIVALS_LIST from './arrivalsList';
+import PRODUCTS_LIST from './productsList';
 
-export const ARRIVALS_LIST = [
-  {
-    name: 'model_1',
-    description: '21WN reversible cardigan',
-    imgUri: require('../../assets/images/model_1.png'),
-    price: 109,
-  },
-  {
-    name: 'model_2',
-    description: 'Green stylish sweater',
-    imgUri: require('../../assets/images/model_2.png'),
-    price: 79,
-  },
-  {
-    name: 'model_3',
-    description: 'Angora gray one piece',
-    imgUri: require('../../assets/images/model_3.png'),
-    price: 159,
-  },
-  {
-    name: 'model_4',
-    description: 'Oblong bag',
-    imgUri: require('../../assets/images/model_4.png'),
-    price: 199,
-  },
-];
+const DUMMY_PAGE_LIST = ['1', '2', '3', '4', '5', '6'];
+const EXLORE_BUTTON_TITLE = 'Explore collection';
+const ARRIVAL_TITLE = 'NEW ARRIVAL';
+const ARRIVAL_CATEGORIES = ['All', 'Apparel', 'Dress', 'Tshirt', 'Bag'];
+const EXPLORE_MORE = 'Explore More';
 
-export const PRODUCTS_LIST = [
-  {
-    name: 'Lamerei',
-    description: 'Recycle Boucle Knit Cardigan Pink',
-    imgUri: require('../../assets/images/product_1.png'),
-    rating: 3.8,
-    price: 120,
-  },
-  {
-    name: 'Naomi',
-    description: 'Green stylish sweater',
-    imgUri: require('../../assets/images/product_2.png'),
-    rating: 2.8,
-    price: 79,
-  },
-  {
-    name: 'Wszystko',
-    description: 'Angora gray one piece',
-    imgUri: require('../../assets/images/product_3.png'),
-    rating: 3.8,
-    price: 159,
-  },
-  {
-    name: 'Merrell',
-    description: 'Oblong bag',
-    imgUri: require('../../assets/images/product_4.png'),
-    rating: 4.2,
-    price: 199,
-  },
-  {
-    name: 'Lamerei',
-    description: 'Recycle Boucle Knit Cardigan Pink',
-    imgUri: require('../../assets/images/product_1.png'),
-    rating: 3.8,
-    price: 120,
-  },
-  {
-    name: 'Naomi',
-    description: 'Green stylish sweater',
-    imgUri: require('../../assets/images/product_2.png'),
-    rating: 2.8,
-    price: 79,
-  },
-  {
-    name: 'Wszystko',
-    description: 'Angora gray one piece',
-    imgUri: require('../../assets/images/product_3.png'),
-    rating: 3.8,
-    price: 159,
-  },
-  {
-    name: 'Merrell',
-    description: 'Oblong bag',
-    imgUri: require('../../assets/images/product_4.png'),
-    rating: 4.2,
-    price: 199,
-  },
-  {
-    name: 'Lamerei',
-    description: 'Recycle Boucle Knit Cardigan Pink',
-    imgUri: require('../../assets/images/product_1.png'),
-    rating: 3.8,
-    price: 120,
-  },
-  {
-    name: 'Naomi',
-    description: 'Green stylish sweater',
-    imgUri: require('../../assets/images/product_2.png'),
-    rating: 2.8,
-    price: 79,
-  },
-  {
-    name: 'Wszystko',
-    description: 'Angora gray one piece',
-    imgUri: require('../../assets/images/product_3.png'),
-    rating: 3.8,
-    price: 159,
-  },
-  {
-    name: 'Merrell',
-    description: 'Oblong bag',
-    imgUri: require('../../assets/images/product_4.png'),
-    rating: 4.2,
-    price: 199,
-  },
-];
+const CONSTANT_DATA = {
+  dummyPageList: DUMMY_PAGE_LIST,
+  exploreButtonTitle: EXLORE_BUTTON_TITLE,
+  arrivalTitle: ARRIVAL_TITLE,
+  arrivalCategories: ARRIVAL_CATEGORIES,
+  exploreMore: EXPLORE_MORE,
+  arrivalsList: ARRIVALS_LIST,
+  productsList: PRODUCTS_LIST,
+};
 
-export const DUMMY_PAGE_LIST = ['1', '2', '3', '4', '5', '6'];
+export default CONSTANT_DATA;
